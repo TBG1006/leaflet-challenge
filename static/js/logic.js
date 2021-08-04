1ba4004655ca31be7a6ae0ca8b054047184ba6ab
Tom Garrity <tomgarrity2020@gmail.com>
2021-08-04 13:42:27 -0400

    colors

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 // Creating map object
 var map = L.map("map", {
-  center: [50, -75.13],
-  zoom: 3,
+  center: [30, -20.13],
+  zoom: 2.5,
 });
 
 // Adding tile layer to the map
@@ -41,17 +41,17 @@ d3.json(queryUrl).then(function (data) {
   function getColor(depth) {
     switch (true) {
       case depth > 90:
-        return "#ea2c2c";
+        return "#000000";
       case depth > 70:
-        return "#ea822c";
+        return "#000080";
       case depth > 50:
-        return "ee9c00";
+        return "#8A2BE2";
       case depth > 30:
-        return "#eecc00";
+        return "#696969";
       case depth > 10:
-        return "#d4ee00";
+        return "#A9A9A9";
       default:
-        return "#98ee00";
+        return "white";
     }
   }
   // this is called above in style function error handl value of zero
@@ -91,7 +91,8 @@ d3.json(queryUrl).then(function (data) {
   legend.onAdd = function() {
     var div = L.DomUtil.create("div", "info legend");
     var grades = [-10, 10, 30, 50, 70, 90];
-    var colors = ["#98ee00", "#d4ee00", "#eecc00", "ee9c00", "ea822c", "#ea2c2c"];
+    // var colors = ["#98ee00", "#d4ee00", "#eecc00", "ee9c00", "ea822c", "#ea2c2c"];
+    var colors = ["white", "#A9A9A9", "#696969", "#8A2BE2", "#000080", "#000000"];
       // labels
     for (var i = 0; i < grades.length; i++) {
       div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
